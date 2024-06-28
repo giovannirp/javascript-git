27b1acb255d40335065565f885c7bc5f373875e5
mucasantos <giovanni_ge@hotmail.com>
2024-06-28 10:30:46 -0300

    adicionando codigo

--- 27-06-2024/sistema-aluno/script.js
+++ 27-06-2024/sistema-aluno/script.js
@@ -1,16 +1,30 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
 
+// Criando uma função para limpar o campo 
+function clearForm() {
+    inputNota.value = "";
+}
+
 // Criando uma função
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(inputNota.value);
+    const inputNotaValue = inputNota.value;
+
+    //Validação de formulário
+    if (inputNotaValue === '') {
+        alert("Campo obrigatório");
+        return false;
+    }
 
     // Lógica
-    if(inputNota.value >= 6) {
+    if(inputNotaValue >= 6) {
         alert("Você está aprovado");
     } else {
         alert("Você está reprovado");
     }
+
+    // CHAMANDO FUNÇÃO DE LIMPAR CAMPO
+    clearForm();
 
 });
